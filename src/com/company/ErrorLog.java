1fa0528f5b210d7a74454b5dfcc78ed707a0b31c
kirtan patel <kirtanpatel@billericak12.com>
2021-12-16 12:00:10 -0500

    r frvfrvfr

--- src/com/company/ErrorLog.java
+++ src/com/company/ErrorLog.java
@@ -7,7 +7,8 @@ public class ErrorLog {
 
     /** Precondition: message is a valid Error log entry */
     public ErrorLog(String message) {
-        /* part a */
+        machineId = message.substring(0,message.indexOf(":"));
+        description =message.substring(message.indexOf(":")+1);
     }
 
     /** Returns true if the description in this error log entry
@@ -15,8 +16,19 @@ public class ErrorLog {
      * Postcondition: the description is unchanged
      */
     public boolean containsKey(String keyword) {
-        /* part b */
-        return false;   // replace this
+       int z = description.indexOf(keyword);
+       if(z==-1){
+           return false;
+       }
+       else if (z ==0){
+           String x = description.substring(keyword.length(), keyword.length()+1);
+           if(x == " "){
+               return true;
+           }
+       }
+       else {
+           return true;
+       }
     }
 
     public String getMachineId() { return machineId;}
@@ -31,14 +43,14 @@ public class ErrorLog {
         ErrorLog er6 = new ErrorLog("Webserver:error on /dev/disk");
         ErrorLog er7 = new ErrorLog("Webserver:disk");
 
-        System.out.println("message 1 " + er1.containsKey("disk"));
-        System.out.println("message 2 " + er2.containsKey("disk"));
-        System.out.println("message 3 " + er3.containsKey("disk"));
-        System.out.println("message 4 " + er4.containsKey("disk"));
-        System.out.println("message 5 " + er5.containsKey("disk"));
-        System.out.println("message 6 " + er6.containsKey("disk"));
-        System.out.println("message 6 " + er6.containsKey("error"));
-        System.out.println("message 7 " + er7.containsKey("disk"));
+        System.out.println("message 1 " + er1.containsKey("disk")); //false
+        System.out.println("message 2 " + er2.containsKey("disk"));//true
+        System.out.println("message 3 " + er3.containsKey("disk"));//false
+        System.out.println("message 4 " + er4.containsKey("disk"));//true
+        System.out.println("message 5 " + er5.containsKey("disk"));//true
+        System.out.println("message 6 " + er6.containsKey("disk"));//false
+        System.out.println("message 6 " + er6.containsKey("error"));// true
+        System.out.println("message 7 " + er7.containsKey("disk"));//true
 
 
     }
